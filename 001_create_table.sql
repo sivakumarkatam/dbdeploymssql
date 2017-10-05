bc1fb52f51fb19321f35cf6e4fb3782739acc323
sivakumarkatam <siva.katam@blazeclan.com>
2017-10-05 15:15:29 +0530

    Create 001_create_table.sql

---
+++ 001_create_table.sql
@@ -0,0 +1,12 @@
+/* Single line comment */
+CREATE TABLE Customers (
+CustomerId smallint identity(1,1),
+Name nvarchar(255),
+Priority tinyint
+)
+CREATE TABLE Sales (
+TransactionId smallint identity(1,1),
+CustomerId smallint,
+[Net Amount] int,
+Completed bit
+);
